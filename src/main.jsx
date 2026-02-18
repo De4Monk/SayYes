f69e3bde12cdd404c8e3ac669d1c309175007cd0
De4Monk <edanilchik@gmail.com>
2026-02-18 19:39:25 +0400

    chore: trigger rebuild with proper build args

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import './index.css'
 import App from './App.jsx'
 import { ErrorBoundary } from './components/utils/ErrorBoundary'
 
+// Build Timestamp: 2026-02-18T19:40:00
+console.log('App Version: 0.0.1 - Build 2');
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ErrorBoundary>
